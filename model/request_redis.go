480c0c94e2c559a1503be295a1eeacbf67b2d3ac
guosongsong <guosongsong@apipost.cn>
2023-03-02 11:32:08 +0800

    docker

--- model/request_redis.go
+++ model/request_redis.go
@@ -70,8 +70,7 @@ func QueryReportData(key string) (value string) {
 }
 
 func InsertHeartbeat(key string, field string, value interface{}) error {
-	result, err := RDB.HSet(key, field, value).Result()
-	log.Logger.Debug("heartbeat插入：      ", result, "      err:", err, "    key:   ", key, "    field:    ", field, "      value:   ", value)
+	_, err := RDB.HSet(key, field, value).Result()
 	return err
 }
 
@@ -81,8 +80,7 @@ func DeleteKey(key string) (err error) {
 }
 
 func InsertMachineResources(key string, value interface{}) error {
-	result, err := RDB.LPush(key, value).Result()
-	log.Logger.Debug("资源插入：      ", result, "      err:", err, "    key:   ", key, "     value:   ", value)
+	_, err := RDB.LPush(key, value).Result()
 	return err
 }
 
