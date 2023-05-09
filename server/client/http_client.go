678846281609fcb91d9c8ac0ca8de9c19c700e30
guosongsong <guosongsong@apipost.cn>
2023-05-09 10:02:32 +0800

    实验：fasthttp如何保持keep-alive

--- server/client/http_client.go
+++ server/client/http_client.go
@@ -10,67 +10,20 @@ import (
 	"github.com/Runner-Go-Team/RunnerGo-engine-open/model"
 	"github.com/valyala/fasthttp"
 	"strings"
+	"sync"
 	"time"
 )
 
-//func HTTPRequest(method, url string, body *model.Body, query *model.Query, header *model.Header, cookie *model.Cookie, auth *model.Auth, httpApiSetup *model.HttpApiSetup) (resp *fasthttp.Response, req *fasthttp.Request, requestTime uint64, sendBytes float64, err error, str string, startTime, endTime time.Time) {
-//
-//	client := fastClient(httpApiSetup, auth)
-//	req = &fasthttp.Request{}
-//
-//	// set method
-//	req.Header.SetMethod(method)
-//	// set header
-//	header.SetHeader(req)
-//	cookie.SetCookie(req)
-//	urls := strings.Split(url, "//")
-//	if !strings.EqualFold(urls[0], model.HTTP) && !strings.EqualFold(urls[0], model.HTTPS) {
-//		url = model.HTTP + "//" + url
-//
-//	}
-//
-//	urlQuery := req.URI().QueryArgs()
-//
-//	if query.Parameter != nil {
-//		for _, v := range query.Parameter {
-//			if v.IsChecked != model.Open {
-//				continue
-//			}
-//			if !strings.Contains(url, v.Key) {
-//				by := v.ValueToByte()
-//				urlQuery.AddBytesV(v.Key, by)
-//				url = url + fmt.Sprintf("&%s=%s", v.Key, string(v.ValueToByte()))
-//			}
-//		}
-//	}
-//	// set url
-//	req.SetRequestURI(url)
-//	// set body
-//	str = body.SetBody(req)
-//
-//	// set auth
-//	auth.SetAuth(req)
-//	resp = &fasthttp.Response{}
-//	startTime = time.Now()
-//	// 发送请求
-//	//if httpApiSetup.IsRedirects == 0 {
-//	//	err = client.DoRedirects(req, resp, httpApiSetup.RedirectsNum)
-//	//} else {
-//	//	err = client.Do(req, resp)
-//	//}
-//	err = client.Do(req, resp)
-//	endTime = time.Now()
-//	requestTime = uint64(time.Since(startTime))
-//	sendBytes = float64(req.Header.ContentLength()) / 1024
-//	if sendBytes <= 0 {
-//		sendBytes = float64(len(req.Body())) / 1024
-//	}
-//	return
-//}
+var (
+	KeepAliveClient *fasthttp.Client
+	once            sync.Once
+)
 
 func HTTPRequest(method, url string, body *model.Body, query *model.Query, header *model.Header, cookie *model.Cookie, auth *model.Auth, httpApiSetup *model.HttpApiSetup) (resp *fasthttp.Response, req *fasthttp.Request, requestTime uint64, sendBytes float64, err error, str string, startTime, endTime time.Time) {
-
-	client := fastClient(httpApiSetup, auth)
+	var client *fasthttp.Client
+	newKeepAlive(httpApiSetup, auth)
+	//client := fastClient(httpApiSetup, auth)
+	client = KeepAliveClient
 	req = fasthttp.AcquireRequest()
 
 	// set method
@@ -183,4 +136,65 @@ func fastClient(httpApiSetup *model.HttpApiSetup, auth *model.Auth) (fc *fasthtt
 	}
 
 	return fc
+}
+
+func newKeepAlive(httpApiSetup *model.HttpApiSetup, auth *model.Auth) {
+	once.Do(func() {
+		tr := &tls.Config{InsecureSkipVerify: true}
+		if auth != nil || auth.Bidirectional != nil {
+			switch auth.Type {
+			case model.Bidirectional:
+				tr.InsecureSkipVerify = false
+				if auth.Bidirectional.CaCert != "" {
+					if strings.HasPrefix(auth.Bidirectional.CaCert, "https://") || strings.HasPrefix(auth.Bidirectional.CaCert, "http://") {
+						client := &fasthttp.Client{}
+						loadReq := fasthttp.AcquireRequest()
+						defer loadReq.ConnectionClose()
+						// set url
+						loadReq.Header.SetMethod("GET")
+						loadReq.SetRequestURI(auth.Bidirectional.CaCert)
+						loadResp := fasthttp.AcquireResponse()
+						defer loadResp.ConnectionClose()
+						if err := client.Do(loadReq, loadResp); err != nil {
+							log.Logger.Error(fmt.Sprintf("机器ip:%s, 下载crt文件失败：", middlewares.LocalIp), err)
+						}
+						if loadResp != nil && loadResp.Body() != nil {
+							caCertPool := x509.NewCertPool()
+							if caCertPool != nil {
+								caCertPool.AppendCertsFromPEM(loadResp.Body())
+								tr.ClientCAs = caCertPool
+							}
+						}
+					}
+				}
+			case model.Unidirectional:
+				tr.InsecureSkipVerify = false
+			}
+		}
+		KeepAliveClient = &fasthttp.Client{
+			Name:                     config.Conf.Http.Name,
+			NoDefaultUserAgentHeader: config.Conf.Http.NoDefaultUserAgentHeader,
+			TLSConfig:                tr,
+			MaxConnsPerHost:          config.Conf.Http.MaxConnPerHost,
+			MaxIdleConnDuration:      5 * time.Minute,
+			//Dial: (&fasthttp.TCPDialer{
+			//	Concurrency:      0,
+			//	DNSCacheDuration: time.Hour,
+			//}).Dial,
+			//MaxIdleConnDuration: config.Conf.Http.MaxIdleConnDuration * time.Millisecond,
+			//MaxConnWaitTimeout:  config.Conf.Http.MaxConnWaitTimeout * time.Millisecond,
+		}
+		log.Logger.Debug("空闲等待连接时长：      ", KeepAliveClient.MaxIdleConnDuration, "    ", KeepAliveClient.MaxConnsPerHost)
+
+		//fc2 := &fasthttp.PipelineClient{
+		//
+		//}
+		if httpApiSetup.WriteTimeOut != 0 {
+			KeepAliveClient.WriteTimeout = time.Duration(httpApiSetup.WriteTimeOut) * time.Millisecond
+		}
+
+		if httpApiSetup.ReadTimeOut != 0 {
+			KeepAliveClient.ReadTimeout = time.Duration(httpApiSetup.ReadTimeOut) * time.Millisecond
+		}
+	})
 }
