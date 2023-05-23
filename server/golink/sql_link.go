d2358594602e38aba29d2ed2767db13f8062d489
guosongsong <guosongsong@apipost.cn>
2023-05-23 18:13:34 +0800

    关于变量优先级的问题

--- server/golink/sql_link.go
+++ server/golink/sql_link.go
@@ -18,32 +18,40 @@ func SqlSend(sql model.SQL, sqlInfo model.MysqlDatabaseInfo, mongoCollection *mo
 	//assertNum       = 0
 	//assertFailedNum = 0
 	)
-
+	isSucceed = true
 	db, result, err, startTime, endTime, requestTime := client.SqlRequest(sqlInfo, sql.SqlString)
 	defer db.Close()
+	if err != nil {
+		isSucceed = false
+	}
 	results := make(map[string]interface{})
 	assertionList := sql.Asser(result)
+	for _, assert := range assertionList {
+		if assert.IsSucceed == false {
+			isSucceed = false
+		}
+	}
 	regex := sql.RegexSql(result, globalVar)
 	if sql.Debug == "all" {
 		results["team_id"] = sql.TeamId
 		results["sql_name"] = sql.Name
 		results["target_id"] = sql.TargetId
 		results["uuid"] = sql.Uuid.String()
-		results["err"] = err
+		if err != nil {
+			results["err"] = err.Error()
+		} else {
+			results["err"] = ""
+		}
+
 		results["request_time"] = requestTime / uint64(time.Millisecond)
 		results["sql_result"] = result
 		results["assertion"] = assertionList
-		if err == nil {
-			isSucceed = true
-			results["status"] = []bool{isSucceed}
-		} else {
-			results["status"] = []bool{isSucceed}
-		}
+		results["status"] = isSucceed
 		by, _ := json.Marshal(sqlInfo)
 		if by != nil {
-			results["database"] = []string{string(by)}
+			results["database"] = string(by)
 		}
-		results["sql"] = []string{sql.SqlString}
+		results["sql"] = sql.SqlString
 		results["regex"] = regex
 	}
 	model.Insert(mongoCollection, results, middlewares.LocalIp)
