4fad4eabadb9cf72fb83c2b3fa6cb2ee47e49c8d
guosongsong <guosongsong@apipost.cn>
2023-06-09 14:41:50 +0800

    新增测试对象dubbo

--- server/golink/dubbo_link.go
+++ server/golink/dubbo_link.go
@@ -19,27 +19,28 @@ func SendDubbo(dubbo model.DubboDetail, mongoCollection *mongo.Collection) {
 	rpcServer, err := client.NewRpcServer(dubbo)
 	if err != nil {
 		results["err"] = err
-		return
-	}
-	parameterTypes, parameterValues := []string{}, []hessian.Object{}
+	} else {
+		parameterTypes, parameterValues := []string{}, []hessian.Object{}
 
-	for _, parame := range dubbo.DubboParam {
-		if parame.IsChecked != model.Open {
-			break
+		for _, parame := range dubbo.DubboParam {
+			if parame.IsChecked != model.Open {
+				break
+			}
+			parameterTypes = append(parameterTypes, parame.ParamType)
+			parameterValues = append(parameterValues, parame.Val)
 		}
-		parameterTypes = append(parameterTypes, parame.ParamType)
-		parameterValues = append(parameterValues, parame.Val)
+
+		results["request_type"] = parameterTypes
+		results["request_body"] = parameterTypes
+		resp, err := rpcServer.(*generic.GenericService).Invoke(
+			context.TODO(),
+			dubbo.FunctionName,
+			parameterTypes,
+			parameterValues, // 实参
+		)
+		results["err"] = err
+		results["response_body"] = resp
 	}
 
-	results["request_type"] = parameterTypes
-	results["request_body"] = parameterTypes
-	resp, err := rpcServer.(*generic.GenericService).Invoke(
-		context.TODO(),
-		dubbo.FunctionName,
-		parameterTypes,
-		parameterValues, // 实参
-	)
-	results["err"] = err
-	results["response_body"] = resp
 	model.Insert(mongoCollection, results, middlewares.LocalIp)
 }
